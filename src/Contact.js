aaca5afcffcdb4c6214d25b25fad8f75c6791709
Marc Salonga <marcandre.salonga931@myci.csuci.edu>
2025-03-22 11:10:40 -0700

    Updated Files for Progress

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <div className="contact-container">
+      <h2>Contact Us</h2>
+      <p>If you have any questions, feel free to reach out!</p>
+      <p>Email: support@lifecouture.com</p>
+      <p>Phone: (123) 456-7890</p>
+      <p>Address: 123 Fitness Lane, Workout City, WC 56789</p>
+    </div>
+  );
+}
+
+export default Contact;
